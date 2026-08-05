517b83a09355f08135c9d746dcfb949354180e8c
Jan Heinrich Merker <heinrich@merker.id>
2026-08-05 15:10:50 +0000

    Re-develop app

--- sommercamp/app.py
+++ sommercamp/app.py
@@ -1,80 +1,95 @@
-
 # Hier importieren wir die benötigten Softwarebibliotheken.
 from os.path import abspath, exists
 from sys import argv
 from streamlit import (text_input, header, title, subheader, 
     container, markdown, link_button, divider, set_page_config)
-from pyterrier import started, init
-# Die PyTerrier-Bibliothek muss zuerst gestartet werden,
-# um alle seine Bestandteile importieren zu können.
-if not started():
-    init()
+
 from pyterrier import IndexFactory
-from pyterrier.batchretrieve import BatchRetrieve
+from pyterrier.terrier import Retriever
 from pyterrier.text import get_text
 
 
-def app(index_dir):
+# Diese Funktion baut die App für die Suche im gegebenen Index auf.
+def app(index_dir) -> None:
+
+    # Konfiguriere den Titel der Web-App (wird im Browser-Tab angezeigt)
     set_page_config(
-        page_title="Braunschweig Suchmaschine",
+        page_title="Bau-Suchmaschine",
         layout="centered",
     )
 
-    title("Braunschweig Suchmaschine")
-    markdown("Willkommen bei der Suchmaschine für die Stadt Braunschweig.")
+    # Gib der App einen Titel und eine Kurzbeschreibung:
+    title("Bau-Suchmaschine")
+    markdown("Hier kannst du unsere neue Bau-Suchmaschine nutzen:")
 
+    # Erstelle ein Text-Feld, mit dem die Suchanfrage (query) 
+    # eingegeben werden kann.
     query = text_input(
-        label="Suche",
-        placeholder="Geben Sie hier Ihren Suchbegriff ein.",
-        value="test",
+        label="Suchanfrage",
+        placeholder="Suche...",
+        value="haus",
     )
 
+    # Wenn die Suchanfrage leer ist, dann kannst du nichts suchen.
     if query == "":
-        markdown("Bitte geben Sie einen Suchbegriff ein.")
+        markdown("Bitte gib eine Suchanfrage ein.")
         return
 
+    # Öffne den Index.
     index = IndexFactory.of(abspath(index_dir))
-
-    searcher = BatchRetrieve(
+    # Initialisiere den Such-Algorithmus. 
+    searcher = Retriever(
         index,
         wmodel="BM25",
         num_results=10,
     )
-    text_getter = get_text(
-        index, metadata=["url", "title", "text"]
-    )
-
+    # Initialisiere das Modul, zum Abrufen der Texte.
+    text_getter = get_text(index, metadata=["url", "title", "text"])
+    # Baue die Such-Pipeline zusammen.
     pipeline = searcher >> text_getter
-
+    # Führe die Such-Pipeline aus und suche nach der Suchanfrage.
     results = pipeline.search(query)
 
+    # Zeige eine Unter-Überschrift vor den Suchergebnissen an.
     divider()
     header("Suchergebnisse")
 
+    # Wenn die Ergebnisliste leer ist, gib einen Hinweis aus.
     if len(results) == 0:
-        markdown("Keine Ergebnisse gefunden.")
+        markdown("Keine Suchergebnisse.")
         return
 
-    markdown(f"Es wurden {len(results)} Ergebnisse gefunden.")
+    # Wenn es Suchergebnisse gibt, dann zeige an, wie viele.
+    markdown(f"{len(results)} Suchergebnisse.")
 
+    # Gib nun der Reihe nach, alle Suchergebnisse aus.
     for _, row in results.iterrows():
+        # Pro Suchergebnis, erstelle eine Box (container).
         with container(border=True):
+            # Zeige den Titel der gefundenen Webseite an.
             subheader(row["title"])
-
+            # Speichere den Text in einer Variablen (text).
             text = row["text"]
+            # Schneide den Text nach 500 Zeichen ab.
             text = text[:500]
+            # Ersetze Zeilenumbrüche durch Leerzeichen.
             text = text.replace("\n", " ")
+            # Zeige den Dokument-Text an.
             markdown(text)
+            # Gib Nutzern eine Schaltfläche, um die Seite zu öffnen.
+            link_button("Seite öffnen", url=row["url"])
 
-            link_button("Öffnen", row["url"])
 
-
+# Die Hauptfunktion, die beim Ausführen der Datei aufgerufen wird.
 def main():
+    # Lade den Pfad zum Index aus dem ersten Kommandozeilen-Argument.
     index_dir = argv[1]
 
+    # Wenn es noch keinen Index gibt, kannst du die Suchmaschine nicht starten.
     if not exists(index_dir):
         exit(1)
 
+    # Rufe die App-Funktion von oben auf.
     app(index_dir)
 
 
